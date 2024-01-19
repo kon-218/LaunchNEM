4ab292e1bc57e2d646e8ac0831c8948fc14c2f7f
Konstantin Nomerotski <kon50@hotmail.co.uk>
2024-01-19 13:15:02 +0000

    comleting TODOs, added error bar options

--- scripts/Workflow/runscript.sh
+++ scripts/Workflow/runscript.sh
@@ -45,15 +45,15 @@ echo -e "Slurm_run finsihed\n"
 # # Extract ORCA outputs
 # "Usage: ./ExtractStates.sh name states istart imax grep_function filesuffix [indicies]"
 # echo "Extracting data from ORCA outputs"
-# ../PROCESS/ExtractStates.sh $name $nstates 1 $nstruct "grep_ORCAUV" "_output.log" ../PROCESS/ $orca_out ../../mol/Spectrum_data/Spectrum_in/ 0
+../PROCESS/ExtractStates.sh $name $nstates 1 $nstruct "grep_ORCAUV" "_output.log" ../PROCESS/ $orca_out ../../mol/Spectrum_data/Spectrum_in/ 0
 # echo -e "\n"
 
 filename=$(find ../../mol/Spectrum_data/Spectrum_in -maxdepth 1 -type f -name "*.exc.txt")
-# echo $filename
-# outdir=../../mol/Spectrum_data/Spectrum_out/
-# workdir=../PROCESS/
-# echo "Building spectrum"
-# sigmaalg="cv"
+echo $filename
+outdir=../../mol/Spectrum_data/Spectrum_out/
+workdir=../PROCESS/
+echo "Building spectrum"
+sigmaalg=cv
 
 # #if [[ anneal -eq 0 ]];then
 # 	#../PROCESS/CalcSpectrum.sh $name $filename $nstruct $nstates 0.02 0.01 false 0 400 $nproc 1 $workdir $outdir 0.05
@@ -61,60 +61,62 @@ filename=$(find ../../mol/Spectrum_data/Spectrum_in -maxdepth 1 -type f -name "*
 # 	#../PROCESS/CalcSpectrum.sh $name $filename $nstruct $nstates 0.025 0.01 false $nsample ncycles $nproc 1 $workdir $outdir 0.05
 # #fi
 # echo $filename $nstruct $nstates
-# ../PROCESS/CalcSpectrumV2.sh $filename $nstruct $nstates 0.02 0.005 false false false 1 $workdir $outdir $sigmaalg 
+ ../PROCESS/CalcSpectrumV2.sh $filename $nstruct $nstates 0.025 0.005 false false false 1 $workdir $outdir $sigmaalg 0.95
 
 # echo "Specrtrum output ready"
-# python3 ../POSTPROCESS/postprocess.py ../../mol/Spectrum_data/Spectrum_out/ absspec..n100000.nm.cross.dat
+ python3 ../POSTPROCESS/postprocess.py ../../mol/Spectrum_data/Spectrum_out/ absspec..n100000.nm.cross.dat
 
 
-for nsample in ${nsamples[@]}; do
-	echo "Running rep sample for $nsample"
-	outdir=../../mol/Spectrum_data/Spectrum_out/out_${nsample}/
-	../PROCESS/repre_sample_2D.py -n $nstruct -N $nstates -S $nsample -c 1 -j 1 -J 1 -w --verbose --outdir $outdir --pdfcomp KLdiv $filename > ../../mol/Spectrum_data/Spectrum_out/output.txt 
+# for nsample in ${nsamples[@]}; do
+# 	echo "Running rep sample for $nsample"
+# 	outdir=../../mol/Spectrum_data/Spectrum_out/out_${nsample}/
+# 	../PROCESS/repre_sample_2D.py -n $nstruct -N $nstates -S $nsample -c 1 -j 1 -J 1 -w --verbose --outdir $outdir --pdfcomp KLdiv $filename > ../../mol/Spectrum_data/Spectrum_out/output.txt 
 
 
-	if [[ anneal -eq 1 ]];then
-		# Extract rep sample geometry indicies
-		echo -e "Extracting rep. sample indicies\n"	
-		#../PROCESS/extract_indicies.sh 
-		mkdir -p ../../mol/Spectrum_data/Spectrum_out/out_$nsample/
-		mkdir -p ../../mol/Spectrum_data/Spectrum_in/annealing_$nsample/
-		mkdir -p ../../mol/ORCA_in/annealing_$nsample/
-		mkdir -p ../../mol/ORCA_out/annealing_$nsample/
+# 	if [[ anneal -eq 1 ]];then
+# 		# Extract rep sample geometry indicies
+# 		echo -e "Extracting rep. sample indicies\n"	
+# 		#../PROCESS/extract_indicies.sh 
+# 		mkdir -p ../../mol/Spectrum_data/Spectrum_out/out_$nsample/
+# 		mkdir -p ../../mol/Spectrum_data/Spectrum_in/annealing_$nsample/
+# 		mkdir -p ../../mol/ORCA_in/annealing_$nsample/
+# 		mkdir -p ../../mol/ORCA_out/annealing_$nsample/
 
-		# Recalc using rep sample geometries
-		echo -e "\nRecreating ORCA input files at higher level of theory\n"
-		indicies_dir=$(find ../../mol/Spectrum_data/Spectrum_out/out_$nsample/ -maxdepth 1 -type f -name "*.geoms.txt")
-		echo $indicies_dir
-		#mkdir -p $indicies_dir
-		orca_in=../../mol/ORCA_in/annealing_$nsample
-		orca_out=../../ORCA_out/annealing_$nsample/
-		method=1
-		../LAUNCH/RecalcGeometries.sh ${name} $orca_in 1 0 ../../mol/geoms.xyz ORCA 1 ${nproc} ../LAUNCH ${nstates} $method $indicies_dir
+# 		# Recalc using rep sample geometries
+# 		echo -e "\nRecreating ORCA input files at higher level of theory\n"
+# 		indicies_dir=$(find ../../mol/Spectrum_data/Spectrum_out/out_$nsample/ -maxdepth 1 -type f -name "*.geoms.txt")
+# 		echo $indicies_dir
+# 		#mkdir -p $indicies_dir
+# 		orca_in=../../mol/ORCA_in/annealing_$nsample
+# 		orca_out=../../ORCA_out/annealing_$nsample/
+# 		method=1
+# 		../LAUNCH/RecalcGeometries.sh ${name} $orca_in 1 0 ../../mol/geoms.xyz ORCA 1 ${nproc} ../LAUNCH ${nstates} $method $indicies_dir
 		
-		# Relaunch ORCA calculations using new inputs
-		# Creates name.output.logs in ORCA_out directory
-		echo -e "\nResubmitting rep sample geometries\n"
-		orca_in=../../mol/ORCA_in/annealing_$nsamples/
-		#../LAUNCH/slurm_run.sh /sw/apps/orca-5.0.3/orca-install/orca $orca_in $orca_out ORCA
+# 		# Relaunch ORCA calculations using new inputs
+# 		# Creates name.output.logs in ORCA_out directory
+# 		echo -e "\nResubmitting rep sample geometries\n"
+# 		orca_in=../../mol/ORCA_in/annealing_$nsamples/
+# 		#../LAUNCH/slurm_run.sh /sw/apps/orca-5.0.3/orca-install/orca $orca_in $orca_out ORCA
 		
-		# Extract high res results
-		orca_out=../../mol/ORCA_out/annealing_$nsample/
-		echo -e "\nExtracting data using rep sample indicies\n"
-		../PROCESS/ExtractStates.sh $name $nstates 1 $nstruct "grep_ORCAUV" "_output.log" ../PROCESS/ $orca_out ../../mol/Spectrum_data/Spectrum_in/annealing/ 0
+# 		# Extract high res results
+# 		orca_out=../../mol/ORCA_out/annealing_$nsample/
+# 		echo -e "\nExtracting data using rep sample indicies\n"
+# 		../PROCESS/ExtractStates.sh $name $nstates 1 $nstruct "grep_ORCAUV" "_output.log" ../PROCESS/ $orca_out ../../mol/Spectrum_data/Spectrum_in/annealing/ 0
 
-		# Recalc high res spectrum, final output
-		outdir=../../mol/Spectrum_data/Spectrum_out/annealing_$nsample/
-		filename=$(find ../../mol/Spectrum_data/Spectrum_in/annealing_${nsample}/ -type f -name "*.exc.txt")
-		echo -e "\nRecalculating rep sample spectrum and outputting\n"
-		#../PROCESS/CalcSpectrum.sh $name $filename $nsample $nstates 0.1 0.01 false 0 400 $nproc 1 $workdir $outdir 0.05
-		echo $filename
-		echo "filename"
-		../PROCESS/CalcSpectrumV2.sh $filename $nsample $nstates 0.035 0.005 false false false 1 $workdir $outdir $sigmaalg 
+# 		# Recalc high res spectrum, final output
+# 		outdir=../../mol/Spectrum_data/Spectrum_out/annealing_$nsample/
+# 		filename=$(find ../../mol/Spectrum_data/Spectrum_in/annealing_${nsample}/ -type f -name "*.exc.txt")
+# 		echo -e "\nRecalculating rep sample spectrum and outputting\n"
+# 		#../PROCESS/CalcSpectrum.sh $name $filename $nsample $nstates 0.1 0.01 false 0 400 $nproc 1 $workdir $outdir 0.05
+# 		echo $filename
+# 		echo "filename"
+# 		../PROCESS/CalcSpectrumV2.sh $filename $nsample $nstates 0.035 0.005 false false false 1 $workdir $outdir $sigmaalg 
 
-		echo -e "\nOuput ready"
-		python3 ../POSTPROCESS/postprocess.py ../../mol/Spectrum_data/Spectrum_out/annealing_$nsample/ absspec..n100000.nm.cross.dat
-	fi
-done
+# 		echo -e "\nOuput ready"
 
-echo "Spectrum build finished"
+#		python3 ../POSTPROCESS/postprocess.py ../../mol/Spectrum_data/Spectrum_out/annealing_$nsample/ absspec..n100000.nm.cross.dat
+# 	fi
+# done
+
+# echo "Spectrum build finished"
+python3 ../POSTPROCESS/post.py 
