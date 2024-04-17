c42bd84b9614eda12fa6e98c11adb294dfe7f530
kon-218 <77125122+kon-218@users.noreply.github.com>
2024-04-17 10:44:36 +0100

    Add files via upload

---
+++ scripts/Workfow/runscript_submit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=PHYS030544 
+#SBATCH --job-name=full
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --mem=100GB
+#SBATCH --time=0:10:00
+#SBATCH --partition=test
+#SBATCH --error=%J.out 
+#SBATCH --output=%J.out
+
+# Load the module environment suitable for the job
+module load apps/orca/5.0.3
+
+which orca
+
+#module list 
+
+echo "running runscript"
+# And finally run the job
+
+./runscript5.sh
+#python3 ../POSTPROCESS/post.py ../../mol/Spectrum_data/Spectrum_out/ ../../mol/Spectrum_data/Spectrum_out/ 5
